f5b537715e3fcab05c3de82f0744bf5105213949
nishitladani-yudiz <63995647+nishitladani-yudiz@users.noreply.github.com>
2020-04-22 18:00:52 +0530

    Added Scene transitions and some sound effects

--- Dots/Assets/Scripts/UIManager.cs
+++ Dots/Assets/Scripts/UIManager.cs
@@ -2,9 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
+    public RectTransform mainMenu;
+
+    void Start()
+    {
+        mainMenu.DOAnchorPos(Vector2.zero, 0.25f);
+    }
     public void TimeScene()
     {
         SceneManager.LoadScene(1);
